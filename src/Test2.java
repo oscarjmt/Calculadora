cca41326131726742ab3312a043e5a629324942b
jeyner777 <jeyner777@gmail.com>
2021-02-06 22:18:01 -0600

    JUnit3

--- src/Test2.java
+++ src/Test2.java
@@ -1,10 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-class Test2 {
+import org.junit.jupiter.api.Test;
 
-	@org.junit.jupiter.api.Test
+/**
+ * Test1
+ * @author Oscar Mendez
+ * @author Jeyner Arango
+ */
+class Test2 {
+	
+	/**
+	 * Test de operaciones de la calculadora
+	 */
+	@Test
 	void test() {
-		fail("Not yet implemented");
+		Calc test2 = new Calc();
+		StackVector<Integer> stackPrueba = new StackVector<Integer>();
+		stackPrueba.push(3);
+		stackPrueba.push("/");
+		assertEquals("Resultado linea 1: 15\nResultado linea 2: 14\nResultado linea 3: 23\n",stack);					
 	}
 
 }
